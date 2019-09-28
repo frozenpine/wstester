3dfb98777089243544b59ed204907c10df1a471b
frozenpine <sonny.frozenpine@gmail.com>
2019-09-28 19:31:08 +0800

    新增server模块

--- server/config.go
+++ server/config.go
@@ -1,7 +1,15 @@
 package server
 
+import "fmt"
+
+// WsConfig websocket listen config
 type WsConfig struct {
 	Listen  string
 	Port    int
 	BaseURI string
+}
+
+// GetListenAddr get configured listen addr for server
+func (c *WsConfig) GetListenAddr() string {
+	return fmt.Sprintf("%s:%d", c.Listen, c.Port)
 }
--- server/server.go
+++ server/server.go
@@ -21,6 +21,20 @@ type Server struct {
 	upgrader *websocket.Upgrader
 }
 
+// RunForever startup and serve forever
+func (s *Server) RunForever() error {
+	http.HandleFunc("/status", s.statusHandler)
+	http.HandleFunc(s.cfg.BaseURI, s.wsHandler)
+
+	err := http.ListenAndServe(s.cfg.GetListenAddr(), nil)
+
+	return err
+}
+
+func (s *Server) wsHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func (s *Server) statusHandler(w http.ResponseWriter, r *http.Request) {
 
 }
