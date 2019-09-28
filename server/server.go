bad9e6f15d8abfdb4f0f700546bb68c8860479a2
frozenpine <sonny.frozenpine@gmail.com>
2019-09-28 16:13:42 +0800

    分离client/server 代码结构

---
+++ server/server.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    4096,
+	WriteBufferSize:   4096,
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+type Server struct {
+	cfg *WsConfig,
+	
+}
