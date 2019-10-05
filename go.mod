9cbf5bfdb62fb07a5a851e1f4207fd08a7ad703d
frozenpine <sonny.frozenpine@gmail.com>
2019-10-05 16:38:44 +0800

    更新模块代码，并完成info接口

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/frozenpine/wstester
 go 1.12
 
 require (
-	github.com/frozenpine/ngerest v0.0.0-20191005082402-84abca0383c1
+	github.com/frozenpine/ngerest v0.0.0-20191005083655-bb8915c5c833
 	github.com/frozenpine/pkcs8 v0.0.0-20190413084423-a8374dc8920d // indirect
 	github.com/gorilla/websocket v1.4.1
 	github.com/spf13/pflag v1.0.5
--- server/server.go
+++ server/server.go
@@ -1,59 +1,147 @@
 package server
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"sync/atomic"
+	"time"
 
+	"github.com/frozenpine/ngerest"
+	"github.com/frozenpine/wstester/models"
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:    4096,
-	WriteBufferSize:   4096,
-	EnableCompression: true,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
+type serverStatics struct {
+	Startup time.Time `json:"startup"`
+	Clients int64     `json:"clients"`
 }
 
 // Status statics for running server
 type Status struct {
-	Clients int `json:"clients"`
+	serverStatics
+
+	Uptime string `json:"uptime"`
 }
 
 // Server server instance
 type Server interface {
-	RunForever() error
+	RunForever(ctx context.Context) error
 }
 
 type server struct {
 	cfg      *WsConfig
 	upgrader *websocket.Upgrader
 
-	status Status
+	statics serverStatics
 }
 
 // RunForever startup and serve forever
-func (s *server) RunForever() error {
+func (s *server) RunForever(ctx context.Context) error {
 	http.HandleFunc("/status", s.statusHandler)
-	http.HandleFunc(s.cfg.BaseURI, s.wsHandler)
+	http.HandleFunc(s.cfg.BaseURI, s.wsUpgrader)
 
+	s.statics.Startup = time.Now().UTC()
 	err := http.ListenAndServe(s.cfg.GetListenAddr(), nil)
 
 	return err
 }
 
-func (s *server) wsHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := upgrader.Upgrade(w, r, w.Header())
+func (s *server) incClients(conn *websocket.Conn) {
+	// remoteAddr := conn.UnderlyingConn().RemoteAddr().String()
+
+	atomic.AddInt64(&s.statics.Clients, 1)
+}
+
+func (s *server) decClients(conn *websocket.Conn) {
+	atomic.AddInt64(&s.statics.Clients, -1)
+}
+
+func (s *server) checkAuthHeader(r *http.Request) error {
+	apiKey := r.Header.Get("api-key")
+	if apiKey == "" {
+
+	}
+
+	apiSignature := r.Header.Get("api-signature")
+	if apiSignature == "" {
+
+	}
+
+	apiExpires, err := strconv.ParseInt(r.Header.Get("api-expires"), 10, 64)
+	if err != nil {
+
+	}
+	if apiExpires > time.Now().Unix() {
+		return NewAPIExpires(apiExpires)
+	}
+
+	return nil
+}
+
+func (s *server) wsUpgrader(w http.ResponseWriter, r *http.Request) {
+	conn, err := s.upgrader.Upgrade(w, r, w.Header())
 
 	if err != nil {
 		http.Error(w, err.Error(), 400)
 	}
+
+	s.incClients(conn)
+
+	defer func() {
+		s.decClients(conn)
+	}()
+
+	info := models.InfoResponse{
+		Info:      "Welcom to NGE websocket service mock.",
+		Version:   "Mock Server v1",
+		Timestamp: ngerest.NGETime(time.Now().UTC()),
+		Docs:      "https://docs.btcmex.com",
+		FrontID:   "0",
+		SessionID: "123456",
+	}
+
+	conn.WriteJSON(info)
+
+	// var (
+	// 	msg []byte
+	// )
+
+	// for {
+	// 	_, msg, err = conn.ReadMessage()
+
+	// 	if err != nil {
+	// 		return
+	// 	}
+	// }
 }
 
 func (s *server) statusHandler(w http.ResponseWriter, r *http.Request) {
-	status, _ := json.Marshal(s.status)
+	status := Status{
+		serverStatics: s.statics,
+		Uptime:        time.Now().Sub(s.statics.Startup).String(),
+	}
+	statusResult, _ := json.Marshal(status)
 
 	w.Header().Set("Content-type", "application/json")
-	w.Write(status)
+	w.Write(statusResult)
+}
+
+//NewServer to create a websocket server
+func NewServer(cfg *WsConfig) Server {
+	svr := server{
+		cfg: cfg,
+		upgrader: &websocket.Upgrader{
+			ReadBufferSize:    4096,
+			WriteBufferSize:   4096,
+			EnableCompression: true,
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+		statics: serverStatics{},
+	}
+
+	return &svr
 }
