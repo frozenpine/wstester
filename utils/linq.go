98d6f88c2334669f7e365a15fa17dce5c6696512
袁暘 <sonny.frozenpine@gmail.com>
2019-10-13 21:57:43 +0800

    完成基础的sql to linq 映射

--- utils/linq.go
+++ utils/linq.go
@@ -7,8 +7,8 @@ import (
 	"strconv"
 	"strings"
 
+	linq "github.com/ahmetb/go-linq"
 	"github.com/frozenpine/ngerest"
-	"github.com/frozenpine/wstester/models"
 	"github.com/xwb1989/sqlparser"
 )
 
@@ -20,6 +20,47 @@ func init() {
 	RegTableModel("trade", new(ngerest.Trade))
 	RegTableModel("instrument", new(ngerest.Instrument))
 	RegTableModel("orderBookL2", new(ngerest.OrderBookL2))
+}
+
+type column struct {
+	name, alias string
+	define      *reflect.StructField
+}
+
+func (col *column) GetName() string {
+	return col.define.Name
+}
+
+func (col *column) GetJSONName() string {
+	jsn, exist := col.define.Tag.Lookup("json")
+
+	if exist {
+		return strings.Split(jsn, ",")[0]
+	}
+
+	return col.GetName()
+}
+
+func (col *column) HasAlias() bool {
+	return col.alias != ""
+}
+
+func (col *column) GetAliasName() string {
+	if col.alias != "" {
+		return col.alias
+	}
+
+	return col.GetName()
+}
+
+func (col *column) GetType() reflect.Type {
+	return col.define.Type
+}
+
+type table struct {
+	name, alias string
+	columns     map[string]*column
+	selected    map[string]*column
 }
 
 // RegTableModel register table module for linq query
@@ -55,8 +96,8 @@ func GetFieldType(data interface{}, property string) *reflect.StructField {
 func GetFields(data interface{}, properties ...string) map[string]interface{} {
 	result := make(map[string]interface{})
 
-	v := reflect.ValueOf(data)
-	t := reflect.TypeOf(data)
+	v := reflect.Indirect(reflect.ValueOf(data))
+	t := reflect.TypeOf(v.Interface())
 
 	for _, prop := range properties {
 		field := v.FieldByName(prop)
@@ -79,6 +120,15 @@ func GetFieldTypes(data interface{}, properties ...string) map[string]*reflect.S
 	t := reflect.TypeOf(data)
 
 	for _, prop := range properties {
+		if prop == "*" {
+			for idx := 0; idx < t.NumField(); idx++ {
+				typ := t.Field(idx)
+
+				result[typ.Name] = &typ
+			}
+
+			break
+		}
 		if typ, exist := t.FieldByName(prop); exist {
 			result[prop] = &typ
 		}
@@ -106,75 +156,99 @@ func parseUnion(stmt sqlparser.Statement) []*sqlparser.Select {
 	return result
 }
 
-func parseTables(stmt *sqlparser.Select) (map[string]*sqlparser.AliasedTableExpr, error) {
-	tableDefine := make(map[string]*sqlparser.AliasedTableExpr)
+func parseTable(stmt *sqlparser.Select) (*table, error) {
+	tableDefine := table{
+		columns: make(map[string]*column),
+	}
 
 	errTableType := errors.New("invalid table type")
 
-	for _, table := range stmt.From {
-		table, ok := table.(*sqlparser.AliasedTableExpr)
-		if !ok {
-			return nil, errTableType
-		}
-
-		tableName, ok := table.Expr.(sqlparser.TableName)
-		if !ok {
-			return nil, errTableType
-		}
-
-		nameStr := tableName.Name.String()
-
-		if _, exist := tableModels[nameStr]; !exist {
-			return nil, errTableType
-		}
-
-		if !table.As.IsEmpty() {
-			nameStr = table.As.String()
-		}
-
-		tableDefine[nameStr] = table
+	if len(stmt.From) > 1 {
+		return nil, errors.New("only one table in select")
 	}
 
-	return tableDefine, nil
+	table, ok := stmt.From[0].(*sqlparser.AliasedTableExpr)
+	if !ok {
+		return nil, errTableType
+	}
+
+	tableName, ok := table.Expr.(sqlparser.TableName)
+	if !ok {
+		return nil, errTableType
+	}
+
+	tableDefine.name = tableName.Name.String()
+	if !table.As.IsEmpty() {
+		tableDefine.alias = table.As.String()
+	}
+
+	model, exist := tableModels[tableDefine.name]
+	if !exist {
+		return nil, errTableType
+	}
+
+	// // v := reflect.Indirect(reflect.ValueOf(model))
+	// t := reflect.TypeOf(model)
+	// // fieldCount := v.NumField()
+	// fieldCount := t.NumField()
+
+	// for idx := 0; idx < fieldCount; idx++ {
+	// 	// def := v.Field(idx)
+
+	// 	col := column{
+	// 		define: t.Field(idx),
+	// 	}
+
+	// 	tableDefine.columns[t.Name()] = &col
+	// }
+
+	for colName, col := range GetFieldTypes(reflect.Indirect(reflect.ValueOf(model)).Interface(), "*") {
+		tableDefine.columns[colName] = &column{
+			name:   colName,
+			define: col,
+		}
+	}
+
+	columns, err := parseColumns(&tableDefine, stmt)
+	if err != nil {
+		return nil, err
+	}
+	tableDefine.selected = columns
+
+	return &tableDefine, nil
 }
 
-func parseColumns(tables map[string]*sqlparser.AliasedTableExpr, stmt *sqlparser.Select) (map[string][]string, error) {
-	columeDefine := make(map[string][]string)
+func parseColumns(tbl *table, stmt *sqlparser.Select) (map[string]*column, error) {
+	var (
+		selectedColumns    = make(map[string]*column)
+		errColumnName      = errors.New("invalid column name")
+		errColumnStatement = errors.New("invalid column statement")
+	)
 
-	for _, column := range stmt.SelectExprs {
-		switch column := column.(type) {
+	for _, col := range stmt.SelectExprs {
+		switch col := col.(type) {
 		case *sqlparser.AliasedExpr:
-			columnName, _ := column.Expr.(*sqlparser.ColName)
+			colName, _ := col.Expr.(*sqlparser.ColName)
 
-			var (
-				tblName string
-			)
-
-			if columnName.Qualifier.IsEmpty() {
-				if len(tables) != 1 {
-					return nil, errors.New("columns has invalid table qualifier")
-				}
-
-				for tblName = range tables {
-					break
-				}
-			} else {
-				tblName = columnName.Qualifier.Name.String()
+			colDef, exist := tbl.columns[colName.Name.String()]
+			if !exist {
+				return nil, errColumnName
+			}
+			if !col.As.IsEmpty() {
+				colDef.alias = col.As.String()
 			}
 
-			if _, exist := tables[tblName]; !exist {
-				return nil, errors.New("columns has no table")
-			}
-
-			columeDefine[tblName] = append(columeDefine[tblName], columnName.Name.String())
+			selectedColumns[colDef.GetName()] = colDef
 		case *sqlparser.StarExpr:
-
+			for _, column := range tbl.columns {
+				selectedColumns[column.GetName()] = column
+			}
 		default:
-			return nil, errors.New("invalid column statement")
+			return nil, errColumnStatement
 		}
 	}
 
-	return columeDefine, nil
+	return selectedColumns, nil
 }
 
 func parseInt(left interface{}, right *sqlparser.SQLVal) (int, int) {
@@ -334,25 +408,52 @@ func parseCondition(expr sqlparser.Expr) (func(v interface{}) bool, error) {
 }
 
 // ParseSQL parse table, column & conditions from SQL
-func ParseSQL(sql string) (map[string]func(models.Response) map[string]interface{}, error) {
+func ParseSQL(sql string) (map[string]func(interface{}) []map[string]interface{}, error) {
 	stmt, err := sqlparser.Parse(sql)
 
 	if err != nil {
 		return nil, err
 	}
 
-	filterFunc := make(map[string]func(models.Response) map[string]interface{})
+	filterFunc := make(map[string]func(interface{}) []map[string]interface{})
 
-	switch stmt := stmt.(type) {
-	case *sqlparser.Select:
-		tableDefine, err := parseTables(stmt)
+	for _, sel := range parseUnion(stmt) {
+		tblDefine, err := parseTable(sel)
+
 		if err != nil {
 			return nil, err
 		}
 
-		parseColumns(tableDefine, stmt)
-	default:
-		return nil, errors.New("sql statement must be SELECT")
+		conditionFn := func(interface{}) bool { return true }
+
+		if sel.Where != nil {
+			conditionFn, err = parseCondition(sel.Where.Expr)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		filterFunc[tblDefine.name] = func(datas interface{}) []map[string]interface{} {
+			var results []map[string]interface{}
+
+			linq.From(datas).Where(func(v interface{}) bool {
+				return conditionFn(v)
+			}).Select(func(v interface{}) interface{} {
+				result := make(map[string]interface{})
+
+				for _, col := range tblDefine.selected {
+					key := col.GetJSONName()
+					if col.HasAlias() {
+						key = col.GetAliasName()
+					}
+					result[key] = GetFieldValue(v, col.GetName())
+				}
+
+				return result
+			}).ToSlice(&results)
+
+			return results
+		}
 	}
 
 	return filterFunc, nil
--- utils/linq_test.go
+++ utils/linq_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/frozenpine/ngerest"
-	"github.com/xwb1989/sqlparser"
 )
 
 func TestGetField(t *testing.T) {
@@ -20,40 +19,46 @@ func TestGetField(t *testing.T) {
 
 func TestParse(t *testing.T) {
 	// sql := "select a.Symbol, b.Price from trade a, instrument b where a.Size > 100 and (b.MarkPrice > 0 or b.FairPrice > 0)"
-	sql := `select Symbol, Price, Size from trade where Price > 1.0 or Symbol = 'XBTUSD' UNION select MarkPrice, FairPrice from instrument union select * from orderBookL2`
+	sql := `select Symbol, Price, Size as volume from trade where Price > 1.0 or Symbol = 'XBTUSD' UNION select MarkPrice, FairPrice from instrument union select * from orderBookL2`
 
 	td := ngerest.Trade{
 		Symbol: "XBTUSD",
 		Price:  0,
 	}
 
-	stmt, err := sqlparser.Parse(sql)
+	// stmt, err := sqlparser.Parse(sql)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	// for _, sel := range parseUnion(stmt) {
+	// 	// tblDefine, err := parseTables(sel)
+	// 	// if err != nil {
+	// 	// 	t.Fatal(err)
+	// 	// }
+	// 	// t.Log(tblDefine)
+
+	// 	// colDefine, err := parseColumns(tblDefine, sel)
+	// 	// if err != nil {
+	// 	// 	t.Fatal(err)
+	// 	// }
+	// 	// t.Log(colDefine)
+
+	// 	if sel.Where != nil {
+	// 		t.Log(sqlparser.String(sel.Where.Expr))
+
+	// 		conditionFn, err := parseCondition(sel.Where.Expr)
+	// 		if err != nil {
+	// 			t.Fatal(err)
+	// 		}
+
+	// 		t.Log(conditionFn(&td))
+	// 	}
+	// }
+	fn, err := ParseSQL(sql)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	for _, sel := range parseUnion(stmt) {
-		tblDefine, err := parseTables(sel)
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Log(tblDefine)
-
-		colDefine, err := parseColumns(tblDefine, sel)
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Log(colDefine)
-
-		if sel.Where != nil {
-			t.Log(sqlparser.String(sel.Where.Expr))
-
-			conditionFn, err := parseCondition(sel.Where.Expr)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			t.Log(conditionFn(&td))
-		}
-	}
+	t.Log(fn["trade"]([]*ngerest.Trade{&td}))
 }
