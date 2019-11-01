20d8e27ec2dbe80aa94c5887b838a80e41b9fef6
frozenpine <sonny.frozenpine@gmail.com>
2019-11-01 18:08:41 +0800

    server端增加日志输出时间精度

--- examples/server/main.go
+++ examples/server/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/frozenpine/wstester/server"
 )
 
 func main() {
+	log.SetFlags(log.Lmicroseconds | log.Ldate)
+
 	cfg := server.NewConfig()
 
 	svr := server.NewServer(nil, cfg)
